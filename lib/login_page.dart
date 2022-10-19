1be5ab34cc685a51b1b9b249e490e78d3970a760
Kushal Prakash <kushal@glimpay.com>
2022-10-19 14:42:58 +0530

    File management sign in

---
+++ lib/login_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool isVisible = false;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  String? uid;
+  String? userEmail;
+  String? name;
+  String? imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/login_01.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+            child: SizedBox(
+                height: 30,
+                width: 230,
+                child: SignInButton(
+                  Buttons.Google,
+                  text: "Sign in with Google",
+                  onPressed: () {
+                    signInWithGoogle();
+                  },
+                ))),
+      ),
+    );
+  }
+
+  signInWithGoogle() async {
+    // Initialize Firebase
+    User? user;
+
+    // The `GoogleAuthProvider` can only be used while running on the web
+    GoogleAuthProvider authProvider = GoogleAuthProvider();
+
+    try {
+      final UserCredential userCredential =
+          await _auth.signInWithPopup(authProvider);
+
+      user = userCredential.user;
+    } catch (e) {
+      print(e);
+    }
+
+    if (user != null) {
+      uid = user.uid;
+      name = user.displayName;
+      userEmail = user.email;
+      imageUrl = user.photoURL;
+
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.setBool('auth', true);
+    }
+
+    print(user);
+  }
+}
