272f1fe20c4bee5d44f93a763711e86f3675f410
Kushal Prakash <kushal@glimpay.com>
2022-10-19 16:33:57 +0530

    UI Design improvements

---
+++ .history/lib/login_page_20221019163017.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool isVisible = false;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  String? uid;
+  String? userEmail;
+  String? name;
+  String? imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          width: MediaQuery.of(context).size.width,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/bg.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            const Text(
+              "Welcome to my File Management App",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Center(
+                child: SizedBox(
+                    height: 30,
+                    width: 230,
+                    child: SignInButton(
+                      Buttons.Google,
+                      text: "Sign in with Google",
+                      onPressed: () {
+                        signInWithGoogle();
+                      },
+                    ))),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              "Sign in with your email and upload your files securely",
+              style: TextStyle(color: Colors.white, fontSize: 15),
+            )
+          ])),
+    );
+  }
+
+  signInWithGoogle() async {
+    // Initialize Firebase
+    User? user;
+
+    // The `GoogleAuthProvider` can only be used while running on the web
+    GoogleAuthProvider authProvider = GoogleAuthProvider();
+
+    try {
+      final UserCredential userCredential =
+          await _auth.signInWithPopup(authProvider);
+
+      user = userCredential.user;
+    } catch (e) {
+      print(e);
+    }
+
+    if (user != null) {
+      uid = user.uid;
+      name = user.displayName;
+      userEmail = user.email;
+      imageUrl = user.photoURL;
+
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.setBool('auth', true);
+      prefs.setString('uid', user.uid);
+      print(user.uid);
+    }
+
+    print(user);
+  }
+}
